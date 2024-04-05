0fbcd55ae3962be6a12ec8ab74c0d93c228af52a
Dogukandogann <flptdogukan@hotmail.com>
2024-04-06 00:01:32 +0300

    Order entities and onion arc. implementation

---
+++ Services/Order/Core/MultiShop.Order.Application/Interfaces/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<List<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task<T> CreateAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task<T> DeleteAsync(T entity);
+        Task<T> GetByFilterAsync(Expression<Func<T,bool>> filter);
+    }
+}
