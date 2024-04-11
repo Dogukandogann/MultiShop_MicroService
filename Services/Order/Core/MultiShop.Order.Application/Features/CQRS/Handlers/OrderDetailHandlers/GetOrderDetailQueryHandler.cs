2ee6490d5bc6108f70f428cd95f0e973eedd840c
Dogukandogann <flptdogukan@hotmail.com>
2024-04-11 23:08:01 +0300

    OrderDetail CQRS

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Handlers/OrderDetailHandlers/GetOrderDetailQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using MultiShop.Order.Application.Features.CQRS.Results.OrderDetailResults;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.CQRS.Handlers.OrderDetailHandlers
+{
+    public class GetOrderDetailQueryHandler
+    {
+        private readonly IRepository<OrderDetail> _orderDetailRepository;
+
+        public GetOrderDetailQueryHandler(IRepository<OrderDetail> orderDetailRepository)
+        {
+            _orderDetailRepository = orderDetailRepository;
+        }
+        public async Task<List<GetOrderDetailQueryResult>> Handle()
+        {
+            var values = await _orderDetailRepository.GetAllAsync();
+            return values.Select(x => new GetOrderDetailQueryResult
+            {
+                OrderDetailId = x.OrderDetailId,
+                OrderingId = x.OrderingId,
+                ProductAmount = x.ProductAmount,
+                ProductId = x.ProductId,
+                ProductName = x.ProductName,
+                ProductPrice = x.ProductPrice,
+                ProductTotalPrice = x.ProductPrice
+            }).ToList();
+        }
+    }
+}
---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Handlers/OrderDetailHandlers/RemoveOrderDetailQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using MultiShop.Order.Application.Features.CQRS.Commands.OrderDetailCommands;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.CQRS.Handlers.OrderDetailHandlers
+{
+    public class RemoveOrderDetailQueryHandler
+    {
+        private readonly IRepository<OrderDetail> _repository;
+
+        public RemoveOrderDetailQueryHandler(IRepository<OrderDetail> repository)
+        {
+            _repository = repository;
+        }
+        public async Task Handle(RemoveOrderDetailCommand removeOrderDetailCommand)
+        {
+            var value = await _repository.GetByIdAsync(removeOrderDetailCommand.Id);
+            await _repository.DeleteAsync(value);
+        }
+    }
+}
