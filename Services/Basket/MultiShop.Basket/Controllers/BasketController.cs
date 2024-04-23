c6f1ea5c2e8b6b91c4f4df412517c9d2f6aee24d
Doğukan Doğan <134203440+Dogukandogann@users.noreply.github.com>
2024-04-23 22:23:22 +0300

    Redis implementation

---
+++ Services/Basket/MultiShop.Basket/Controllers/BasketController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Basket.Dtos;
+using MultiShop.Basket.LoginServices;
+using MultiShop.Basket.Services;
+
+namespace MultiShop.Basket.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BasketController : ControllerBase
+    {
+        private readonly IBasketService _basketService;
+        private readonly ILoginService _loginService;
+
+        public BasketController(IBasketService basketService, ILoginService loginService)
+        {
+            _basketService = basketService;
+            _loginService = loginService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetBasketDetail()
+        {
+            var user = User.Claims;
+            var values = await _basketService.GetBasket(_loginService.GetUserId);
+            return Ok(values);
+        }
+        [HttpPost]
+        public async Task<IActionResult> SaveBasket(BasketTotalDto basketTotalDto)
+        {
+            basketTotalDto.UserId = _loginService.GetUserId;
+            await _basketService.SaveBasket(basketTotalDto);
+            return Ok("Sepet Kayıt Edildi");
+        }
+        [HttpDelete]
+        public async Task<IActionResult> DeleteBasket()
+        {
+            await _basketService.DeleteBasket(_loginService.GetUserId);
+            return Ok("Sepet Başarıyla Silindi");
+        }
+    }
+}
