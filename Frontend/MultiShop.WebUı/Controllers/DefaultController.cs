5adbcf0089436dec090c4c2a73d345ad25c79a9b
Doğukan Doğan <134203440+Dogukandogann@users.noreply.github.com>
2024-04-24 23:24:33 +0300

    MultiShop layout

---
+++ Frontend/MultiShop.WebUı/Controllers/DefaultController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUı.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Frontend/MultiShop.WebUı/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MultiShop.WebUı.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
