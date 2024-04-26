d60e0ae7c9093877b08fbf6a79cad37215e0583f
Doğukan Doğan <134203440+Dogukandogann@users.noreply.github.com>
2024-04-26 22:22:52 +0300

    User  interface uı partials added.

---
+++ Frontend/MultiShop.WebUı/ViewComponents/DefaultVievComponents/_OfferDiscountComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUı.ViewComponents.DefaultVievComponents
+{
+    public class _OfferDiscountComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ Frontend/MultiShop.WebUı/ViewComponents/UıLayoutViewComponents/_FooterUıLayoutComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace MultiShop.WebUı.ViewComponents.UıLayoutViewComponents
+{
+    public class _FooterUıLayoutComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
