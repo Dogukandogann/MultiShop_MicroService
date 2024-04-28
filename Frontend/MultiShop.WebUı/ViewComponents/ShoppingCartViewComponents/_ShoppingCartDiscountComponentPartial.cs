69aaa38564dadc128765b8b08ab4d37dd33e4f1b
Doğukan Doğan <134203440+Dogukandogann@users.noreply.github.com>
2024-04-28 13:38:15 +0300

    Shopping cart and contact controller added (Admin layout implemented to project docs)

---
+++ Frontend/MultiShop.WebUı/ViewComponents/ShoppingCartViewComponents/_ShoppingCartDiscountComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUı.ViewComponents.ShoppingCartViewComponents
+{
+    public class _ShoppingCartDiscountComponentPartial :ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
