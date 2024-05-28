bf34aef22c5697192c165a003a8baf7d397c539f
Dogukandogann <dogukn.dogann@gmail.com>
2024-05-28 22:52:57 +0300

    Token request test

--- Frontend/MultiShop.WebUı/Controllers/TestController.cs
+++ Frontend/MultiShop.WebUı/Controllers/TestController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MultiShop.DtoLayer.CatalogDtos.CategoryDtos;
 using Newtonsoft.Json;
+using System.Text.Json.Nodes;
 
 namespace MultiShop.WebUı.Controllers
 {
@@ -14,6 +15,30 @@ namespace MultiShop.WebUı.Controllers
 		}
 		public async Task<IActionResult> Index()
 		{
+			string token;
+			using (var httpClient = new HttpClient())
+			{
+				var request = new HttpRequestMessage
+				{
+					RequestUri = new Uri("https://localhost:5001/connect/token"),
+					Method = HttpMethod.Post,
+					Content = new FormUrlEncodedContent(new Dictionary<string, string>
+					{
+						{"client_id","MultiShopVisitorId" },
+						{"client_secret","multishopsecret" },
+						{"grant_type","client_credentials" }
+					})
+				};
+				using (var response = await httpClient.SendAsync(request))
+				{
+					if (response.IsSuccessStatusCode) 
+					{ 
+						var content = await response.Content.ReadAsStringAsync();
+						var tokenResponse = JsonObject.Parse(content);
+						token = tokenResponse["access_token"].ToString();
+					}
+				}
+			}
 			var client = _httpClientFactory.CreateClient();
 			var responseMessage = await client.GetAsync("https://localhost:7070/api/Categories");
 			if (responseMessage.IsSuccessStatusCode)
