d3e0764ae194e4e7a69da9947179bd31eb2fb9f3
Dogukandogann <dogukn.dogann@gmail.com>
2024-05-30 22:22:53 +0300

    token

--- Frontend/MultiShop.WebUı/ViewComponents/UıLayoutViewComponents/_NavbarUıLayoutComponentPartial.cs
+++ Frontend/MultiShop.WebUı/ViewComponents/UıLayoutViewComponents/_NavbarUıLayoutComponentPartial.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MultiShop.DtoLayer.CatalogDtos.CategoryDtos;
 using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Text.Json.Nodes;
 
 namespace MultiShop.WebUı.ViewComponents.UıLayoutViewComponents
 {
@@ -16,7 +18,33 @@ namespace MultiShop.WebUı.ViewComponents.UıLayoutViewComponents
         public async Task<IViewComponentResult> InvokeAsync()
         {
 
+            string token = "";
+            using (var httpClient = new HttpClient())
+            {
+                var request = new HttpRequestMessage
+                {
+                    RequestUri = new Uri("https://localhost:5001/connect/token"),
+                    Method = HttpMethod.Post,
+                    Content = new FormUrlEncodedContent(new Dictionary<string, string>
+                    {
+                        {"client_id","MultiShopVisitorId" },
+                        {"client_secret","multishopsecret" },
+                        {"grant_type","client_credentials" }
+                    })
+                };
+                using (var response = await httpClient.SendAsync(request))
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var content = await response.Content.ReadAsStringAsync();
+                        var tokenResponse = JsonObject.Parse(content);
+                        token = tokenResponse["access_token"].ToString();
+                    }
+                }
+            }
             var client = _httpClientFactory.CreateClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
             var responseMessage = await client.GetAsync("https://localhost:7070/api/Categories");
             if (responseMessage.IsSuccessStatusCode)
             {
