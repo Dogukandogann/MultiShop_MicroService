37d62ff1dbce63a4503008fe84c237e1bdca6cbe
Dogukandogann <dogukn.dogann@gmail.com>
2024-06-03 22:09:15 +0300

    Services api settings

--- Frontend/MultiShop.WebUı/Controllers/LoginController.cs
+++ Frontend/MultiShop.WebUı/Controllers/LoginController.cs
@@ -31,46 +31,9 @@ namespace MultiShop.WebUı.Controllers
 		[HttpPost]
 		public async Task<IActionResult> Index(CreateLoginDto createLoginDto)
 		{
-			
-			var client = _httpClientFactory.CreateClient();
-			var content = new StringContent(JsonSerializer.Serialize(createLoginDto), Encoding.UTF8, "application/json");
-			var response = await client.PostAsync("https://localhost:5001/api/Logins", content);
-			if (response.IsSuccessStatusCode)
-			{
-				var jsonData = await response.Content.ReadAsStringAsync();
-				var tokenModel = JsonSerializer.Deserialize<JwtResponseModel>(jsonData, new JsonSerializerOptions
-				{
-					PropertyNamingPolicy = JsonNamingPolicy.CamelCase
-				});
-				if (tokenModel is not null)
-				{
-					JwtSecurityTokenHandler hander = new JwtSecurityTokenHandler();
-					var token = hander.ReadJwtToken(tokenModel.Token);
-					var claims = token.Claims.ToList();
-
-                    if (tokenModel.Token is not null)
-                    {
-						claims.Add(new Claim("MultiShopToken",tokenModel.Token));
-						var claimsIdentity = new ClaimsIdentity(claims,JwtBearerDefaults.AuthenticationScheme);
-						var authProps = new AuthenticationProperties{ 
-						ExpiresUtc= tokenModel.ExpireDate,
-						IsPersistent = true
-						};
-						await HttpContext.SignInAsync(JwtBearerDefaults.AuthenticationScheme,new ClaimsPrincipal(claimsIdentity),authProps);
-						var id = _loginService.GetUserId;
-						return RedirectToAction("Index","Default");
-                    }
-                }
-			}
 			return View();
-
 		}
 
-		//[HttpGet]
-		//public IActionResult SignIn()
-		//{
-		//	return View();
-		//}
 		//[HttpPost]
 		public async Task<IActionResult> SignIn(SignInDto signInDto)
 		{
--- Frontend/MultiShop.WebUı/Services/Interfaces/IIdentityService.cs
+++ Frontend/MultiShop.WebUı/Services/Interfaces/IIdentityService.cs
@@ -5,5 +5,6 @@ namespace MultiShop.WebUı.Services.Interfaces
     public interface IIdentityService
     {
         Task<bool> SignIn(SignInDto signInDto);
+        Task<bool> GetRefreshToken();
     }
 }
