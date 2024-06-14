3fae6e7dbe08e36242a2db8f4da71f6b0c058729
Dogukandogann <dogukn.dogann@gmail.com>
2024-06-14 21:50:04 +0300

    OrderViewComponents added

---
+++ Frontend/MultiShop.WebUı/ViewComponents/OrderViewComponents/_PaymentMethodComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUı.ViewComponents.OrderViewComponents
+{
+    public class _PaymentMethodComponentPartial :ViewComponent
+    {
+        public IViewComponentResult Invoke() 
+        { 
+            return View(); 
+        }
+    }
+}
