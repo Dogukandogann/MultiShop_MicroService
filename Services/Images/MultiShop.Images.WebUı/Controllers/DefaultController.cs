15c76db1ec0efb8edd45eabd41d45d03b82a25de
Dogukandogann <dogukn.dogann@gmail.com>
2024-06-28 14:27:51 +0300

    feat(Images): The image service has been activated through Google Cloud.

---
+++ Services/Images/MultiShop.Images.WebUı/Controllers/DefaultController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.Images.WebUı.Dal.Entities;
+using MultiShop.Images.WebUı.Services;
+
+namespace MultiShop.Images.WebUı.Controllers
+{
+    public class DefaultController : Controller
+    {
+        private readonly ICloudStorageService _cloudStorageService;
+
+        public DefaultController(ICloudStorageService cloudStorageService)
+        {
+            _cloudStorageService = cloudStorageService;
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ImageDrive ımageDrive)
+        {
+            
+           if (ımageDrive.Photo != null)
+           {
+             ımageDrive.SavedFileName = GenerateFileNameToSave(ımageDrive.Photo.FileName);
+             ımageDrive.SavedUrl = await _cloudStorageService.UploadFileAsync(ımageDrive.Photo, ımageDrive.SavedFileName);
+           }
+            return RedirectToAction("Index","Default");
+        }
+
+        private string? GenerateFileNameToSave(string incomingFileName)
+        {
+            var fileName = Path.GetFileNameWithoutExtension(incomingFileName);
+            var extension = Path.GetExtension(incomingFileName);
+            return $"{fileName}-{DateTime.Now.ToUniversalTime().ToString("yyyyMMddHHmmss")}{extension}";
+        }
+
+    }
+}
