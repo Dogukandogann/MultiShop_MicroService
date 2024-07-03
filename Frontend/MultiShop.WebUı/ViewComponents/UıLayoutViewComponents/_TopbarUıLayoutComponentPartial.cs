0064f386a9191f031889f3a699a5cf6d1e575d3a
Dogukandogann <dogukn.dogann@gmail.com>
2024-07-03 22:10:06 +0300

    feat(RapidApi):Rapidapi api implemented for uı

--- Frontend/MultiShop.WebUı/ViewComponents/UıLayoutViewComponents/_TopbarUıLayoutComponentPartial.cs
+++ Frontend/MultiShop.WebUı/ViewComponents/UıLayoutViewComponents/_TopbarUıLayoutComponentPartial.cs
@@ -1,12 +1,42 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.RapidApiWebUı.Models;
+using Newtonsoft.Json;
 
 namespace MultiShop.WebUı.ViewComponents.UıLayoutViewComponents
 {
     public class _TopbarUıLayoutComponentPartial :ViewComponent
     {
-        public IViewComponentResult Invoke()
+        public async Task<IViewComponentResult> InvokeAsync()
         {
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Add("x-rapidapi-key", "a8c1779fe2msh0bc9ca135007d6ap166f35jsn5a1f1a9b8a42");
+            client.DefaultRequestHeaders.Add("x-rapidapi-host", "real-time-finance-data.p.rapidapi.com");
+
+            var usdRequestUri = new Uri("https://real-time-finance-data.p.rapidapi.com/currency-exchange-rate?from_symbol=USD&language=en&to_symbol=TRY");
+            var eurRequestUri = new Uri("https://real-time-finance-data.p.rapidapi.com/currency-exchange-rate?from_symbol=EUR&language=en&to_symbol=TRY");
+
+            var usdTask = GetExchangeRateAsync(client, usdRequestUri);
+            var eurTask = GetExchangeRateAsync(client, eurRequestUri);
+
+            await Task.WhenAll(usdTask, eurTask);
+
+            var usdValues = await usdTask;
+            var eurValues = await eurTask;
+
+            ViewBag.ExchangeUsd = usdValues.data.exchange_rate;
+            ViewBag.ExchangeEur = eurValues.data.exchange_rate;
             return View();
+        }
+        private async Task<CurrencyViewModel.Rootobject> GetExchangeRateAsync(HttpClient client, Uri requestUri)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, requestUri);
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<CurrencyViewModel.Rootobject>(body);
+            }
+
         }
     }
 }
